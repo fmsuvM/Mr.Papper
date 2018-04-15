bb6d7e8423a8b757d08c037b3970f3f4942a34d8
fmsuvM <k222.tkd.fms@gmail.com>
2018-04-15 20:09:40 +0900

    Feat: add redux devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, Store } from 'redux';
 import { Provider } from 'react-redux';
-import * as qs from 'qs';
 import * as Debug from 'debug';
 
 import reducers from './reducers/index';
 import epicsMiddleware from './middleware/index';
 import loader from './utils/PDFLoader';
+import { RootState } from './models/model';
 
 import Home from './components/Home';
 
 const debug = Debug('Mr.Papper::App');
-const isDev = qs.parse(location.search.replace('?', ''))['isDev'] || false;
-debug(qs.parse(location.search.replace('?', ''))['isDev']);
 
 window.addEventListener('DOMContentLoaded', () => {
     debug('=====> Mount App');
-    // const composeEnhances = isDev
-    //     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    //     : compose;
-    const store = createStore(reducers, {});
+    const initialState = {};
+    const enhancer = window['devToolsExtension']
+        ? window['devToolsExtension']()(createStore)
+        : createStore;
+    const store: Store<any> = enhancer(reducers, initialState);
     loader.init(store);
     ReactDOM.render(
         <Provider store={store}>
