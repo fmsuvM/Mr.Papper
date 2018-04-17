9120b2a5729fa16dfc0a6a63e7cca67eda047785
fmsuvM <k222.tkd.fms@gmail.com>
2018-04-17 22:47:11 +0900

    Feat: change to SFC Component from Statefill Component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Dispatch, bindActionCreators, Action } from 'redux';
+import { bindActionCreators, Action } from 'redux';
 import * as Debug from 'debug';
 
 import { RootState } from '../models/model';
@@ -10,29 +10,23 @@ import Button from './Button';
 const debug = Debug('Mr.Papper::Component::Home');
 
 interface HomeProps {
-    hoge?: string;
-    test?: () => any;
+    hoge: string;
+    test: () => any;
 }
 
-class Home extends React.Component<HomeProps, {}> {
-    render() {
-        return (
-            <div>
-                <Button primary={false}>Normal Button</Button>
-                <Button primary={true}> Primary Button</Button>
-            </div>
-        );
-    }
-}
+export const Home: React.SFC<HomeProps> = props => {
+    const { hoge, test } = props;
+    const handleTest = () => {
+        debug('handle test is successed');
+        test();
+    };
 
-const mapStateToProps = (state: RootState) => ({
-    requestData: state.requestData
-});
-
-// const mapDispatchToProps = (dispatch: Dispatch<Action>) => ({
-//     test: () => {
-//         dispatch(testTrigger);
-//     }
-// });
-
-export default connect(mapStateToProps, null)(Home);
+    return (
+        <div>
+            <Button primary={false}>Normal Button {hoge}</Button>
+            <Button primary={true} onClick={handleTest}>
+                {`Primary Button`}
+            </Button>
+        </div>
+    );
+};
