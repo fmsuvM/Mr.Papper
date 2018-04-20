062a99a0f1e9358e0a2404bc418951943c853bab
fmsuvM <k222.tkd.fms@gmail.com>
2018-04-21 01:20:05 +0900

    Feat: add hide/show list button

--- src/components/List.js
+++ src/components/List.js
@@ -16,26 +16,42 @@ const dialogOptions = {
 class List extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            open: false
+        };
+        this.open = trigger => () => {
+            this.setState({
+                open: !trigger
+            });
+        };
     }
 
     render() {
         const papers = this.props.papers;
         debug('papers: ', papers);
+        let categorizedPage = null;
+        categorizedPage = (
+            <div>
+                <ul>
+                    {papers.map((paper, key) => {
+                        return <li key={key}>{paper}</li>;
+                    })}
+                </ul>
+            </div>
+        );
         return (
             <div>
                 <p>リストを表示</p>
                 {this.props.isLoading ? (
                     <p>Now Loading...</p>
                 ) : (
-                    <ul>
-                        {papers.map((paper, key) => {
-                            debug('paper: ', paper);
-                            debug('key', key);
-                            return <li key={key}>{paper}</li>;
-                        })}
-                    </ul>
+                    <div>
+                        {this.state.open ? <div>{categorizedPage}</div> : null}
+                        <Button onClick={this.open(this.state.open)}>
+                            開く
+                        </Button>
+                    </div>
                 )}
-                <Button onClick={this.props.select}>フォルダ選択ボタン</Button>
             </div>
         );
     }
@@ -44,7 +60,8 @@ class List extends React.Component {
 List.propTypes = {
     select: PropTypes.func,
     papers: PropTypes.array,
-    isLoading: PropTypes.bool
+    isLoading: PropTypes.bool,
+    open: PropTypes.func
 };
 
 const mapStateToProps = state => ({
@@ -59,7 +76,8 @@ const mapDispatchToProps = dispatch => ({
             debug('response', res);
             dispatch(selectData(res));
         });
-    }
+    },
+    open: () => {}
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);
