31c7f58964ad79934c58147302aae4c823ebdd88
fmsuvM <k222.tkd.fms@gmail.com>
2018-05-05 22:56:34 +0200

    Feat: split modal component from PaperList Comopnet

--- src/components/PaperList.js
+++ src/components/PaperList.js
@@ -2,31 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Debug from 'debug';
-import ReactModal from 'react-modal';
 
-import {
-    registPaper,
-    closePaperModal,
-    receivePaperInfo
-} from '../actions/index';
+import { registPaper } from '../actions/index';
 import Button from '../UIcomponents/Button';
-import PaperEditForm from './PaperEditForm';
+import PaperEditModal from './PaperEditModal';
 
 const debug = Debug('Mr.Papper::List::');
-
-const modalStyles = {
-    content: {
-        width: '700px',
-        height: '400px',
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-        overlfow: 'scroll'
-    }
-};
 
 class PaperList extends React.Component {
     constructor(props) {
@@ -52,13 +33,6 @@ class PaperList extends React.Component {
         this.registeringPaper = filename => () => {
             this.props.registeringPaper(filename);
         };
-        this.closePaperModal = () => {
-            this.props.closePaperModal();
-        };
-        this.submitPaperStatus = status => {
-            status.originname = this.props.targetPaper;
-            this.props.submitPaperStatus(status);
-        };
     }
 
     render() {
@@ -71,12 +45,11 @@ class PaperList extends React.Component {
             paperList = (
                 <div>
                     <ul>
-                        {' '}
                         {paper.map((_paper, key) => {
                             const{ title } = _paper;
                             return <li key={key}> {title} </li>;
-                        })}{' '}
-                    </ul>{' '}
+                        })}
+                    </ul>
                 </div>
             );
         }
@@ -87,77 +60,56 @@ class PaperList extends React.Component {
             unknownList = (
                 <div>
                     <ul>
-                        {' '}
                         {unknown.map((_unknown, key) => {
                             return (
                                 <li key={key}>
-                                    {' '}
-                                    {_unknown}{' '}
+                                    {_unknown}
                                     <p
                                         onClick={this.registeringPaper(
                                             _unknown
                                         )}
                                     >
-                                        論文を登録するボタン{' '}
-                                    </p>{' '}
+                                        論文を登録するボタン
+                                    </p>
                                 </li>
                             );
-                        })}{' '}
-                    </ul>{' '}
+                        })}
+                    </ul>
                 </div>
             );
         }
         return (
             <div>
-                <p> 論文リスト </p>{' '}
+                <p> 論文リスト </p>
                 {this.props.isLoading ? (
                     <p> Now Loading... </p>
                 ) : (
                     <div>
                         <div>
-                            {' '}
                             {this.state.openPaper ? (
                                 <div> {paperList} </div>
-                            ) : null}{' '}
+                            ) : null}
                             <Button
                                 onClick={this.openPaper(this.state.openPaper)}
                             >
-                                論文リストを開く{' '}
-                            </Button>{' '}
-                        </div>{' '}
+                                論文リストを開く
+                            </Button>
+                        </div>
                         <div>
-                            {' '}
                             {this.state.openUnknown ? (
                                 <div> {unknownList} </div>
-                            ) : null}{' '}
+                            ) : null}
                             <Button
                                 onClick={this.openUnknown(
                                     this.state.openUnknown
                                 )}
                             >
-                                未分類リストを開く{' '}
-                            </Button>{' '}
-                        </div>{' '}
+                                未分類リストを開く
+                            </Button>
+                        </div>
                     </div>
-                )}{' '}
-                {this.props.isShowModal ? (
-                    <ReactModal
-                        isOpen={this.props.isShowModal}
-                        style={modalStyles}
-                        onRequestClose={this.closePaperModal}
-                        contentLabel="delete confirmation"
-                        ariaHideApp={false}
-                    >
-                        <h2> 論文登録開始 </h2>{' '}
-                        <p> paper name is {this.props.targetPaper} </p>{' '}
-                        <PaperEditForm
-                            onSubmit={values => this.submitPaperStatus(values)}
-                        />{' '}
-                        <Button onClick={this.closePaperModal}> close </Button>{' '}
-                    </ReactModal>
-                ) : (
-                    <span />
-                )}{' '}
+                )}
+                {this.props.isShowModal ? <PaperEditModal /> : <span />}
             </div>
         );
     }
@@ -171,9 +123,7 @@ PaperList.propTypes = {
     open: PropTypes.func,
     registeringPaper: PropTypes.func,
     isShowModal: PropTypes.bool,
-    targetPaper: PropTypes.string,
-    closePaperModal: PropTypes.func,
-    submitPaperStatus: PropTypes.func
+    targetPaper: PropTypes.string
 };
 
 const mapStateToProps = state => ({
@@ -187,13 +137,6 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     registeringPaper: filename => {
         dispatch(registPaper(filename));
-    },
-    closePaperModal: () => {
-        dispatch(closePaperModal());
-    },
-    submitPaperStatus: status => {
-        debug('status: ', status);
-        dispatch(receivePaperInfo(status));
     }
 });
 
